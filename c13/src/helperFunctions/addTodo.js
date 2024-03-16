5351a38c1f92b39961a8a4e22fe764f95c0b3193
johnson-oragui <johnson-oragui@gmail.com>
2024-03-16 16:26:47 +0100

    re-arranged for proper structure

--- c13/src/helperFunctions/addTodo.js
+++ c13/src/helperFunctions/addTodo.js
@@ -5,12 +5,16 @@ const addTodo = async (userId, todoData) => {
     // Create or update the todo item associated with the userId
     const todo = await Todo.findOneAndUpdate(
       { userId }, // Search for todo items by userId
-      todoData, // Update with the new todoData
+      {
+        userId: todoData.userId,
+        title: todoData.sanitizedTitle,
+        todo: todoData.sanitizedTodo,
+      }, // Update with the new todoData
       { upsert: true, new: true }, // Options to create if not found and return the updated document
     );
     return todo;
   } catch (error) {
-    console.error('error in addTodo, cannot add todo: ', error.message);
+    console.error('error in  addTodo, cannot add todo: ', error.message);
     throw new Error('Failed to add todo');
   }
 };
