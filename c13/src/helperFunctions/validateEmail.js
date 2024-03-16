cca4d90f433165677b40b011f98b1553606dfa93
johnson-oragui <johnson-oragui@gmail.com>
2024-03-16 13:35:12 +0100

    function to enforce email format

---
+++ c13/src/helperFunctions/validateEmail.js
@@ -0,0 +1,7 @@
+const validateEmail = (email) => {
+  // Regular expression for basic email format validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+module.exports = validateEmail;
