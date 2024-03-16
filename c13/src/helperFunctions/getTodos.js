e51a5dd1b7937e4526ae2c855d7c1794f0b253d7
johnson-oragui <johnson-oragui@gmail.com>
2024-03-16 16:27:57 +0100

    added sorting atomic method to sort todos

--- c13/src/helperFunctions/getTodos.js
+++ c13/src/helperFunctions/getTodos.js
@@ -3,7 +3,7 @@ const Todo = require('../models/todoModel');
 const getTodos = async (userId) => {
   try {
     // find the todo items associated with the userId
-    const todos = await Todo.find({ userId });
+    const todos = await Todo.find({ userId }, { $sort: { createdAt: -1 } });
     return todos;
   } catch (error) {
     console.error('error in getTodos cannot add todo: ', error.message);
