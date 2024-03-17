a5730d09852f7ee2166d815d88a7ac750b957743
johnson-oragui <johnson-oragui@gmail.com>
2024-03-17 18:23:54 +0100

    modified to include a multer file proccess middleware

--- c13/src/routes/profile.js
+++ c13/src/routes/profile.js
@@ -2,14 +2,18 @@ const express = require('express');
 const ProfileController = require('../controllers/profileController');
 const authenticationMiddleware = require('../middlewares/authMiddleware');
 const { updateProfileRateLimit } = require('../middlewares/rateLimitMiddleware');
+const upload = require('../config/uploadFileMulterConfig');
 
 const profileRoute = express.Router();
 
 // handles profile route for updating user profile name, email, and phone number
-profileRoute.put('/api/todo/:userId', updateProfileRateLimit, authenticationMiddleware, ProfileController.putProfile);
+// /api/todo/userId/profile - PUT: Update the profile details of the authenticated user.
+profileRoute.put('/api/todo/:userId/profile', updateProfileRateLimit, authenticationMiddleware, ProfileController.putProfile);
 // handles the update/change of user profile password
+// /api/todo/userId/password - PUT: Update the password of the authenticated user.
 profileRoute.put('/api/todo/:userId/password', updateProfileRateLimit, authenticationMiddleware, ProfileController.putPassword);
 // handles the update/change of user profile avatar
-profileRoute.put('/api/todo/:userId/avatar', updateProfileRateLimit, authenticationMiddleware, ProfileController.putAvatar);
+// /api/todo/userId/avatar - PUT: Update the avatar of the authenticated user.
+profileRoute.put('/api/todo/:userId/avatar', updateProfileRateLimit, authenticationMiddleware, upload.single('avatar'), ProfileController.putAvatar);
 
 module.exports = profileRoute;
