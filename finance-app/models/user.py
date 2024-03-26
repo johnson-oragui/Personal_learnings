09ca8f547c90d241f3858aa5e500ed1a428ef2a5
johnson-oragui <johnson-oragui@gmail.com>
2024-03-26 13:04:56 +0100

    moved the check_password function from cls methods

--- finance-app/models/user.py
+++ finance-app/models/user.py
@@ -1,27 +1,37 @@
 from bson import ObjectId
 import bcrypt
 
-class User:
-    def __init__(self, first_name, last_name, email, phone_number, username, password, account_type):
-      self.id = ObjectId()
-      self.first_name = first_name
-      self.last_name = last_name
-      self.email = email
-      self.phone_number = phone_number
-      self.username = username
-      self.password = self._hash_password(password)
-      self.account_type = account_type
+def check_password(password: str, db_pwd) -> bool:
+    """
+    Compare the password
+    """
+    # Check if the provided password matches the hashed password
+    return bcrypt.checkpw(password.encode('utf-8'), db_pwd.encode('utf-8'))
 
-    def _hash_password(self, password):
+class User:
+    """
+    User class that represents a user
+    """
+    def __init__(self, user_data: dict) -> None:
+      self.id = ObjectId()
+      self.first_name = user_data.get('first_name')
+      self.last_name = user_data.get('last_name')
+      self.email = user_data.get('email')
+      self.phone_number = user_data.get('phone_number')
+      self.username = user_data.get('username')
+      self.password = self._hash_password(user_data.get('password'))
+      self.account_type = user_data.get('account_type')
+
+    def _hash_password(self, password: str) -> str:
+        """
+        Hash password
+        """
         # Hash the password securely using bcrypt
         return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-    def check_password(self, password):
-        # Check if the provided password matches the hashed password
-        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
-
-    def to_dict(self):
+    def to_dict(self) -> dict:
         """
+        COnverts to a dictionary
         """
         return {
           "_id": self.id,
