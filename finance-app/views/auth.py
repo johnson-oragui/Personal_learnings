39a83d7ba8f06786ba794ddcdfe9d979e560efe5
johnson-oragui <johnson-oragui@gmail.com>
2024-03-26 12:59:20 +0100

    completed logic for registration, login, and logout

--- finance-app/views/auth.py
+++ finance-app/views/auth.py
@@ -1,11 +1,11 @@
-from flask import request, render_template, flash, redirect, url_for
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
+from os import getenv
+from flask import request, render_template, flash, redirect, url_for, make_response
+from flask_jwt_extended import create_access_token, jwt_required, set_access_cookies, unset_jwt_cookies
+from datetime import timedelta
 from . import auth_views
-from utils.helperFunc.db_helpers import DB
-from utils.helperFunc.user_inputs_helpers import *
-from models.user import User
+from utils.mongodb_manager import DB
+from utils.user_inputs_manager import *
+from models.user import *
 
 
 
@@ -14,6 +14,7 @@ def register():
     """
     Register new user
     """
+    # create an instance of mongodb manager
     db = DB()
     if request.method == 'POST':
         first_name = request.form.get('first_name')
@@ -34,13 +35,31 @@ def register():
             "password": password,
             "account_type": account_type
         }
-        if not (password == password2):
-            flash('Both passwords must be the same', 'error')
-            return render_template('register.html', inputs=inputs)
+        
         # validate user inputs are not empty, returns true if not empty
         user_inputs: bool = validate_user_inputs(inputs)
         if not user_inputs:
             flash('All fields must be filled', 'error')
+            return render_template('register.html', inputs=inputs)
+        
+        # define allowed characters for first, last, and user name.
+        # returns boolean indicating whether the field has a xter allowed
+        try:
+            fn, ln, us = allowed_characters({"first_name": first_name,
+                                            "last_name": last_name,
+                                            "username": username
+                                            })
+        except ValueError:
+            flash('COuld not proccess the form, try again', 'error')
+            return render_template('register.html', inputs=inputs)
+        if not fn:
+            flash('Only alphabets allowed for first name', 'error')
+            return render_template('register.html', inputs=inputs)
+        if not ln:
+            flash('Only alphabets allowed for last name', 'error')
+            return render_template('register.html', inputs=inputs)
+        if not us:
+            flash('Only alphabets, numbers, underscore, and hyphen allowed', 'error')
             return render_template('register.html', inputs=inputs)
 
         # validate email format
@@ -56,17 +75,24 @@ def register():
             flash('Email already exists', 'error')
             return render_template('register.html', inputs=inputs)
         
+        # vaalidate phone number format
+        phone_number_validate = validate_phone_number(phone_number)
+        if not phone_number_validate:
+            flash('Invalid phone number format', 'error')
+            return render_template('register.html', inputs=inputs)
+        
         # check if email already exists in database
         username_exists = db.find_user_by_username(username)
         if username_exists:
             print('user exist')
             flash('username already exists', 'error')
             return render_template('register.html', inputs=inputs)
-
-        # vaalidate phone number format
-        pone_number_validate = validate_phone_number(phone_number)
-        if not pone_number_validate:
-            flash('Invalid phone number format', 'error')
+        
+        if not (password == password2):
+            flash('Both passwords must be the same', 'error')
+            return render_template('register.html', inputs=inputs)
+        if len(password) < 6:
+            flash('password must be up to six characters', 'error')
             return render_template('register.html', inputs=inputs)
 
         # validate length of username, and password
@@ -74,15 +100,29 @@ def register():
             flash('Username must be up to 4 characters', 'error')
             return render_template('register.html', inputs=inputs)
         
-        # sanitized user inputs
+        try:
+            # sanitized user inputs against xss, csrf
+            user_data = xssSanitizer(inputs)
+        except Exception:
+            flash(f'form contains not allowed characters', 'error')
+            return render_template('register.html', inputs=inputs)
+        print('user_data: ', user_data)
+        try:
+            user = User(user_data)
+        except Exception as e:
+            print('failed to create user object', e)
+            flash('User could not be registered', 'error')
+            return render_template('register.html', inputs=inputs)
 
-        user = User(first_name, last_name, email, phone_number, username, password, account_type)
-
-        new_user = db.create_user(user.to_dict())
-        if not new_user:
+        try:
+            user_id, account_id = db.create_user_with_transaction(user.to_dict())
+            if user_id and account_id:
+                return redirect(url_for('auth_views.login'))
+        except Exception as e:
+            print('except: ', e)
             flash('User could not be created, try again', 'error')
             return render_template('register.html', inputs=inputs)
-        return redirect(url_for('/login'))
+        
     return render_template('register.html')
 
 
@@ -91,16 +131,51 @@ def login():
     """
     Login new user
     """
+    db = DB()
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
         print(username)
         print(password)
-        return render_template('login.html')
+        try:
+            validated_inputs = validate_user_inputs({"username": username,
+                                                     "password": password})
+            if not validated_inputs:
+                flash('All fields must not be empty', 'error')
+                return render_template('login.html', username=username)
+        except Exception:
+            flash('Could not validate inputs', 'error')
+            return render_template('login.html', username=username)
+        try:
+            username_exists = db.find_user_by_username(username)
+        except :
+            flash('Could not validate username', 'error')
+            return render_template('login.html', username=username)
+        if not username_exists:
+            print('incorrect username/password')
+            flash('incorrect username/password', 'error')
+            return render_template('login.html', username=username)
+        pwd_check = check_password(password, username_exists.get('password'))
+        if not pwd_check:
+            print('wrong password')
+            flash('incorrect username/password', 'error')
+            return render_template('login.html', username=username)
+        identity = {
+            "_id": str(username_exists.get('_id')),
+            "username": username_exists.get('username')
+        }
+        access_token = create_access_token(identity=identity, additional_claims={"ie": getenv("JWT_ADDITIONAL_CLAIMS"), "audience": getenv("JWT_AUDIENCE")})
+        # print('access_token: ', access_token)
+        
 
-        # validate user inputs are not empty
+        response = make_response(render_template('dashboard.html'))
 
-        # sanitized user inputs
+
+        set_access_cookies(response, access_token)
+        print('access_token: ', access_token)
+
+        return response
+
     return render_template('login.html')
 
 
@@ -110,4 +185,7 @@ def logout():
     """
     Logout new user
     """
-    return render_template('login.html')
+    response = redirect(url_for('login'))
+    # response.headers['Authorization'] = ''
+    unset_jwt_cookies(response)
+    return response
